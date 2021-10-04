eb556f8fb1d0f56116df1b24e7d42b8e322a8c75
Erick Amoedo <erick.amoedo@live.com>
2021-10-04 14:39:56 -0300

    Conceito de estado para minha aplicação

---
+++ src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export function Counter() {
+  // Aqui estou desestruturando com []
+  const [counter, setCounter] = useState(0);
+
+  function Increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <h2>{counter}</h2>
+      <button type="button" onClick={Increment}>
+        incrementar
+      </button>
+    </div>
+  );
+}

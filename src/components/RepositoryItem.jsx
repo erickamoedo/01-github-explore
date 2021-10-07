b7a1fd6380d5b60d820757fe84b236a591f22dc7
Erick Amoedo <erick.amoedo@live.com>
2021-10-07 10:34:47 -0300

    consumindo API da songsterr

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,11 +1,22 @@
 export function RepositoryItem(props) {
   return (
+    /*  API do songsterr */
     <li>
-      <strong>{props.repository.name ?? "Vazio"}</strong>
-      <p> {props.repository.description ?? "vazio"} </p>
+      <strong>{props.repository.title ?? "Vazio"}</strong>
+      <p> {props.repository.artist.nameWithoutThePrefix ?? "vazio"} </p>
       <a href={props.repository.html_url ?? "vazio"} target="_blank">
-        {props.repository.html_url ?? "vazio"}
+        {props.repository.id ?? "vazio"}
       </a>
     </li>
+
+    /*  API do GIT
+      <li>
+        <strong>{props.repository.name ?? "Vazio"}</strong>
+        <p> {props.repository.description ?? "vazio"} </p>
+        <a href={props.repository.html_url ?? "vazio"} target="_blank">
+          {props.repository.html_url ?? "vazio"}
+        </a>
+      </li>
+    */
   );
 }
